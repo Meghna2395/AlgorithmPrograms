78aa39e620f51f338e034f1890781c5c2d27896d
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-24 16:09:00 +0530

    Add files via upload

---
+++ Permutation.java
@@ -0,0 +1,24 @@
+package com.bridzelabz.Algorithms;
+
+public class Permutation {
+
+	public static void main(String[] args) {
+	     findPermutation("","MEGH");
+	     
+	}
+	
+	public static  void findPermutation(String initialString, String inputString) {
+		int length = inputString.length();
+		
+		if (inputString.isEmpty()) {
+			System.out.println(initialString+ inputString);
+		}
+		else {
+			for(int i = 0; i < inputString.length();i++) {
+				findPermutation(initialString+inputString.charAt(i),
+				inputString.substring(0,i)+inputString.substring(i+1,length));
+			}
+		}
+	}
+
+}
