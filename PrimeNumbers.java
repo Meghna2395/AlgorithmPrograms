707cdde13cccbe61e390a5846f8312952ab61666
Meghna2395 <66842750+Meghna2395@users.noreply.github.com>
2021-07-25 15:31:07 +0530

    Add files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,26 @@
+package com.bridzelabz.Algorithms;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int temp = 0;
+		// Empty String
+		String primeNumbers = "";
+
+		for (i = 1; i <= 1000; i++) {
+			int a = 0;
+			for (temp = i; temp >= 1; temp--) {
+				if (i % temp == 0) {
+					a = a + 1;
+				}
+			}
+			if (a == 2) {
+				// Appended the Prime number to the String
+				primeNumbers = primeNumbers + i + " ";
+			}
+		}
+		System.out.println("Prime numbers from 1 to 1000 are :");
+		System.out.println(primeNumbers);
+	}
+}
